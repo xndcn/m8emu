6418c16fab05c13aac380a2283c9fe4499693765
xndcn <xndchn@gmail.com>
2024-12-17 16:52:28 +0800

    Add multi-threading for audio processor

--- src/config.cpp
+++ src/config.cpp
@@ -25,24 +25,44 @@ bool FirmwareConfig::LoadConfig(const std::string& path, const std::string& firm
         *config = YAML::LoadFile(path);
     }
     // TODO: Add hash check here
-    std::string firmware_name = std::filesystem::path(firmware).filename();
-    if (!(*config)[firmware_name]) {
-        ext::LogError("Failed to parse firmware %s", firmware_name.c_str());
+    firmwareName = std::filesystem::path(firmware).filename();
+    if (!(*config)[firmwareName]) {
+        ext::LogError("Failed to parse firmware %s", firmwareName.c_str());
         return false;
     }
-    *config = (*config)[firmware_name];
 
-    for (const auto& symbol : (*config)["symbols"]) {
+    for (const auto& symbol : (*config)[firmwareName]["symbols"]) {
         const auto& name = symbol.first.as<std::string>();
         const auto& addr = symbol.second.as<u32>();
         symbols[name] = addr;
     }
+    for (const auto& range : (*config)[firmwareName]["ranges"]) {
+        const auto& name = range.first.as<std::string>();
+        ranges[name] = {range.second[0].as<u32>(), range.second[1].as<u32>()};
+    }
     return true;
 }
+
+template<class T> T FirmwareConfig::GetValue(const std::string& key)
+{
+    if ((*config)[firmwareName]["configs"][key]) {
+        return (*config)[firmwareName]["configs"][key].as<T>();
+    } else if ((*config)["*"]["configs"][key]) {
+        return (*config)["*"]["configs"][key].as<T>();
+    }
+    return {};
+}
+
+template u32 FirmwareConfig::GetValue<u32>(const std::string& key);
 
 u32 FirmwareConfig::GetSymbolAddress(const std::string& symbol)
 {
     return symbols[symbol];
+}
+
+std::tuple<u32, u32> FirmwareConfig::GetEntryRange(const std::string& entry)
+{
+    return ranges[entry];
 }
 
 } // namespace m8
