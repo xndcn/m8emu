7e0956948a059e5272ffae7ed18daf70b0e43749
xndcn <xndchn@gmail.com>
2024-12-17 16:52:28 +0800

    Add multi-threading for audio processor

--- src/config.h
+++ src/config.h
@@ -16,12 +16,17 @@ class FirmwareConfig {
 public:
     static FirmwareConfig& GlobalConfig();
     bool LoadConfig(const std::string& path, const std::string& firmware);
+    template<class T> T GetValue(const std::string& key);
     u32 GetSymbolAddress(const std::string& symbol);
+    std::tuple<u32, u32> GetEntryRange(const std::string& entry);
 
 private:
     FirmwareConfig();
 
+    std::string firmwareName;
     std::map<std::string, u32> symbols;
+    std::map<std::string, std::tuple<u32, u32>> ranges;
+
     std::shared_ptr<YAML::Node> config;
 };
 
